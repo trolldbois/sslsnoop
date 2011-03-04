fd37189030b52e8405bcb86ac4d8caec534cb699
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2011-03-03 19:22:39 -0700

    nice and quick

--- abouchet.py
+++ abouchet.py
@@ -6,10 +6,17 @@
 
 __author__ = "Loic Jaquemet loic.jaquemet+python@gmail.com"
 
-import os,logging,re
+import os,logging,sys
 #use volatility?
 
-import ssl
+import model 
+import ctypes
+from ctypes import memmove,byref
+
+# linux only
+from ptrace.debugger.debugger import PtraceDebugger
+from ptrace.debugger.memory_mapping import readProcessMappings
+
 
 
 log=logging.getLogger('abouchet')
@@ -17,72 +24,25 @@ MAX_KEYS=255
 
 verbose = 0
 
-# memory extraction functions {{{ 
 
-''' returns 0 if the address seems to be valid
-          -1 else.
 '''
-def is_valid_address(addr,proc):
-'''static int is_valid_address(unsigned long addr, proc_t *proc) {'''
-  #if ( ((long)addr) & (sizeof(char *)-1) )
-  #  return -1 ;
-
- return not sdbg_map_lookup_by_address(proc, addr, None)
-
-
-''' returns 0/True if all addresses seems to be valid
-          -1/False else.
-'''
-def might_be_RSA(rsa, proc):
-  '''static int might_be_RSA(RSA *rsa, proc_t *proc) {'''
-    return 
-	rsa->pad || rsa->version ||
-	(rsa->references < 0) || (rsa->references > 0xff)  ||
-        is_valid_address( (unsigned long) rsa->n, proc)    ||
-        is_valid_address( (unsigned long) rsa->e, proc)    ||
-        is_valid_address( (unsigned long) rsa->d, proc)    ||
-        is_valid_address( (unsigned long) rsa->p, proc)    ||
-        is_valid_address( (unsigned long) rsa->q, proc)    ||
-        is_valid_address( (unsigned long) rsa->dmp1, proc) ||
-        is_valid_address( (unsigned long) rsa->dmq1, proc) ||
-        is_valid_address( (unsigned long) rsa->iqmp, proc);
-}
-
-/* returns 0 if all addresses seems to be valid
-          -1 else.
-*/
-static int might_be_DSA(DSA *dsa, proc_t *proc) {
-    return
-        dsa->pad || dsa->version ||
-        (dsa->references < 0) || (dsa->references > 0xff)  ||
-        is_valid_address( (unsigned long) dsa->p, proc)        ||
-        is_valid_address( (unsigned long) dsa->q, proc)        ||
-        is_valid_address( (unsigned long) dsa->g, proc)        ||
-        is_valid_address( (unsigned long) dsa->priv_key, proc) ||
-        is_valid_address( (unsigned long) dsa->pub_key, proc)  ||
-       //  pre-calc -> may be NULL
-        (dsa->kinv && is_valid_address( (unsigned long) dsa->kinv, proc)) ||
-        (dsa->r && is_valid_address( (unsigned long) dsa->r, proc));
-}
-
-
 static void *extract_from_mem(void *addr, unsigned int size, proc_t *proc) {
-	mapping_t *map;
-	unsigned int off;
+  mapping_t *map;
+  unsigned int off;
 
-	map = dbg_map_lookup_by_address(proc, (xaddr_t)addr, &off);
-	if (!map || !map->data)
-		return NULL;
+  map = dbg_map_lookup_by_address(proc, (xaddr_t)addr, &off);
+  if (!map || !map->data)
+    return NULL;
 
-	return map->data + off;
+  return map->data + off;
 }
 
 static int is_valid_BN(BIGNUM *bn) {
     
     printf("BN { d=%p, top=%i, dmax=%i, neg=%i, flags=%i }\n",
-		bn->d, bn->top, bn->dmax, bn->neg, bn->flags);
-	  if ( bn->dmax < 0 || bn->top < 0 || bn->dmax < bn->top )
-				return -1;
+    bn->d, bn->top, bn->dmax, bn->neg, bn->flags);
+    if ( bn->dmax < 0 || bn->top < 0 || bn->dmax < bn->top )
+        return -1;
 
     if ( !(bn->neg == 1 || bn->neg == 0 ) ) {
         return -1;
@@ -105,37 +65,37 @@ static int is_valid_BN(BIGNUM *bn) {
    */
 
 BIGNUM * BN_extract_from_mem(void * bn_addr, proc_t * proc, int * error) {
-	BIGNUM *bn_tmp;
+  BIGNUM *bn_tmp;
 
-	*error = 0;
+  *error = 0;
 //printf("=a= %p\n", bn_addr);
-	bn_tmp = extract_from_mem( bn_addr, sizeof(BIGNUM), proc);
-	if (!bn_tmp) {
+  bn_tmp = extract_from_mem( bn_addr, sizeof(BIGNUM), proc);
+  if (!bn_tmp) {
 //printf("=z=\n");
-		*error = 1;
-		return NULL;
-	}
-	BIGNUM *bn = malloc(sizeof(BIGNUM));
-	if ( bn == NULL ) {
-		exit(EXIT_FAILURE);
-	}
-	memcpy(bn, bn_tmp, sizeof(BIGNUM));
-	/* tests heuristiques */
-	if ( is_valid_BN(bn) == -1 ) { 
-			*error = 1;
-			free(bn);
-			return NULL;
-	}
+    *error = 1;
+    return NULL;
+  }
+  BIGNUM *bn = malloc(sizeof(BIGNUM));
+  if ( bn == NULL ) {
+    exit(EXIT_FAILURE);
+  }
+  memcpy(bn, bn_tmp, sizeof(BIGNUM));
+  /* tests heuristiques */
+  if ( is_valid_BN(bn) == -1 ) { 
+      *error = 1;
+      free(bn);
+      return NULL;
+  }
 
 //printf("=b=\n");
-	bn->d = extract_from_mem( bn->d, bn->top * sizeof(BN_ULONG), proc);
-	if (!bn->d) {
-		*error = 1;
-		free(bn);
-		return NULL;
-	}
+  bn->d = extract_from_mem( bn->d, bn->top * sizeof(BN_ULONG), proc);
+  if (!bn->d) {
+    *error = 1;
+    free(bn);
+    return NULL;
+  }
 //printf("=c=\n");
-	return bn;
+  return bn;
 }
 /* }}} */
 
@@ -213,16 +173,16 @@ int extract_rsa_key(RSA *rsa, proc_t *p) {
 
     int error = 0;
 
-		if ( verbose > 1 ) {
-				printf("RSA { pad=%i, ver=%li, ref=%i, flags=%i, engine=%p\n"
-								"      n=%p, e=%p, d=%p, p=%p, q=%p\n"    
-								"      dmp1=%p, dmq1=%p, iqmp=%p, bn_data=%p\n"
-								"      blinding=%p, mont_bn=%p/%p/%p }\n",
-								rsa->pad, rsa->version, rsa->references, rsa->flags, rsa->engine,
-								rsa->n, rsa->e, rsa->d, rsa->p, rsa->q, rsa->dmp1, rsa->dmq1,
-								rsa->iqmp, rsa->bignum_data, rsa->blinding,
-								rsa->_method_mod_n, rsa->_method_mod_p, rsa->_method_mod_q);
-		}
+    if ( verbose > 1 ) {
+        printf("RSA { pad=%i, ver=%li, ref=%i, flags=%i, engine=%p\n"
+                "      n=%p, e=%p, d=%p, p=%p, q=%p\n"    
+                "      dmp1=%p, dmq1=%p, iqmp=%p, bn_data=%p\n"
+                "      blinding=%p, mont_bn=%p/%p/%p }\n",
+                rsa->pad, rsa->version, rsa->references, rsa->flags, rsa->engine,
+                rsa->n, rsa->e, rsa->d, rsa->p, rsa->q, rsa->dmp1, rsa->dmq1,
+                rsa->iqmp, rsa->bignum_data, rsa->blinding,
+                rsa->_method_mod_n, rsa->_method_mod_p, rsa->_method_mod_q);
+    }
 
     rsa->n =    BN_extract_from_mem(rsa->n, p, &error);
     if ( error ) return -1;
@@ -241,28 +201,28 @@ int extract_rsa_key(RSA *rsa, proc_t *p) {
     rsa->iqmp = BN_extract_from_mem(rsa->iqmp, p, &error);
     if ( error ) { goto free_dmq1; }
 
-	rsa->meth = NULL;
-	rsa->_method_mod_n = NULL;
-	rsa->_method_mod_p = NULL;
-	rsa->_method_mod_q = NULL;
-	rsa->bignum_data = NULL;
-	rsa->blinding = NULL;
+  rsa->meth = NULL;
+  rsa->_method_mod_n = NULL;
+  rsa->_method_mod_p = NULL;
+  rsa->_method_mod_q = NULL;
+  rsa->bignum_data = NULL;
+  rsa->blinding = NULL;
 //#if OPENSSL_VERSION_NUMBER >
-	//rsa->mt_blinding = NULL;
+  //rsa->mt_blinding = NULL;
 //#endif
 
-	 switch ( RSA_check_key( rsa )) {
-		 case 1 :
-			 return 0;
-		 case 0 :
-			 if (verbose > 1)
-				 fprintf(stderr, "warn: invalid RSA key found.\n");
-			 break;
-		 case -1 :
-			 if (verbose > 1)
-				 fprintf(stderr, "warn: unable to check key.\n");
-			 break;
-	 }
+   switch ( RSA_check_key( rsa )) {
+     case 1 :
+       return 0;
+     case 0 :
+       if (verbose > 1)
+         fprintf(stderr, "warn: invalid RSA key found.\n");
+       break;
+     case -1 :
+       if (verbose > 1)
+         fprintf(stderr, "warn: unable to check key.\n");
+       break;
+   }
 
     free(rsa->iqmp);
 free_dmq1 :
@@ -283,285 +243,191 @@ free_n :
     return -1 ;
 }
 
-void free_rsa_key(RSA *rsa) {
-    free(rsa->iqmp);
-    free(rsa->dmq1);
-    free(rsa->dmp1);
-    free(rsa->q);
-    free(rsa->p);
-    free(rsa->d);
-    free(rsa->e);
-    free(rsa->n);
-}
-/*
-   struct
-   {
-   BIGNUM *p;              // prime number (public)
-   BIGNUM *q;              // 160-bit subprime, q | p-1 (public)
-   BIGNUM *g;              // generator of subgroup (public)
-   BIGNUM *priv_key;       // private key x
-   BIGNUM *pub_key;        // public key y = g^x
-// ...
-}
-DSA;
-*/
 
 int extract_dsa_key( DSA * dsa, proc_t *p ) {
-	int error;
+  int error;
 
-	if ( verbose > 1 ) {
-			printf("DSA { pad=%i, ver=%li, ref=%i, flags=%i, engine=%p\n"
-							"      p=%p, q=%p, g=%p, pubkey=%p, pvkey=%p\n"    
-							"      kinv=%p, r=%p, mont_p=%p, meth=%p }\n",
-							dsa->pad, dsa->version, dsa->references, dsa->flags, dsa->engine,
-							dsa->p, dsa->q, dsa->g, dsa->pub_key, dsa->priv_key, dsa->kinv,
-							dsa->r, dsa->method_mont_p, dsa->meth);
-	}
+  if ( verbose > 1 ) {
+      printf("DSA { pad=%i, ver=%li, ref=%i, flags=%i, engine=%p\n"
+              "      p=%p, q=%p, g=%p, pubkey=%p, pvkey=%p\n"    
+              "      kinv=%p, r=%p, mont_p=%p, meth=%p }\n",
+              dsa->pad, dsa->version, dsa->references, dsa->flags, dsa->engine,
+              dsa->p, dsa->q, dsa->g, dsa->pub_key, dsa->priv_key, dsa->kinv,
+              dsa->r, dsa->method_mont_p, dsa->meth);
+  }
 
-	dsa->priv_key = BN_extract_from_mem(dsa->priv_key, p, &error);
-	if ( error ) return -1;
+  dsa->priv_key = BN_extract_from_mem(dsa->priv_key, p, &error);
+  if ( error ) return -1;
 
-	dsa->pub_key = BN_extract_from_mem(dsa->pub_key, p, &error);
-	if ( error ) goto free_priv_key;
-	dsa->p = BN_extract_from_mem(dsa->p, p, &error);
-	if ( error ) goto free_pub_key;
-	dsa->q = BN_extract_from_mem(dsa->q, p, &error);
-	if ( error ) goto free_p;
-	dsa->g = BN_extract_from_mem(dsa->g, p, &error);
-	if ( error ) goto free_q;
-	dsa->kinv = BN_extract_from_mem(dsa->kinv, p, &error);
-	if ( error ) dsa->kinv = NULL;
-	dsa->r = BN_extract_from_mem(dsa->r, p, &error);
-	if ( error ) dsa->r = NULL;
+  dsa->pub_key = BN_extract_from_mem(dsa->pub_key, p, &error);
+  if ( error ) goto free_priv_key;
+  dsa->p = BN_extract_from_mem(dsa->p, p, &error);
+  if ( error ) goto free_pub_key;
+  dsa->q = BN_extract_from_mem(dsa->q, p, &error);
+  if ( error ) goto free_p;
+  dsa->g = BN_extract_from_mem(dsa->g, p, &error);
+  if ( error ) goto free_q;
+  dsa->kinv = BN_extract_from_mem(dsa->kinv, p, &error);
+  if ( error ) dsa->kinv = NULL;
+  dsa->r = BN_extract_from_mem(dsa->r, p, &error);
+  if ( error ) dsa->r = NULL;
 
-	dsa->method_mont_p = NULL;
-	dsa->meth = NULL;
-	dsa->engine = NULL;
+  dsa->method_mont_p = NULL;
+  dsa->meth = NULL;
+  dsa->engine = NULL;
 
-	/* in DSA, we should have :
-	 * pub_key = g^priv_key mod p
-	 */
-	BIGNUM * res = BN_new();
-	if ( res == NULL )
-		err(p, "failed to allocate result BN");
+  /* in DSA, we should have :
+   * pub_key = g^priv_key mod p
+   */
+  BIGNUM * res = BN_new();
+  if ( res == NULL )
+    err(p, "failed to allocate result BN");
 
-	BN_CTX * ctx = BN_CTX_new();
-	if ( ctx == NULL ) {
-		fprintf(stderr, "[-] error allocating BN_CTX ctx\n");
-		goto free_res;
-	}
-	/* a ^ p % m
-		int BN_mod_exp(BIGNUM *r, BIGNUM *a, const BIGNUM *p,
-		const BIGNUM *m, BN_CTX *ctx);
-		*/
-	error = BN_mod_exp(res, dsa->g, dsa->priv_key, dsa->p, ctx);
-	if ( error == 0 ) {
-		if (verbose > 0)
-			fprintf(stderr, "warn: failed to check DSA key.\n");
-		goto free_ctx;
-	}
-	if ( BN_cmp(res, dsa->pub_key) != 0 ) {
-		if (verbose > 0)
-			fprintf(stderr, "warn: invalid DSA key.\n");
-		goto free_ctx;
-	}
-	BN_clear_free(res);
-	BN_CTX_free(ctx);
+  BN_CTX * ctx = BN_CTX_new();
+  if ( ctx == NULL ) {
+    fprintf(stderr, "[-] error allocating BN_CTX ctx\n");
+    goto free_res;
+  }
+  /* a ^ p % m
+    int BN_mod_exp(BIGNUM *r, BIGNUM *a, const BIGNUM *p,
+    const BIGNUM *m, BN_CTX *ctx);
+    */
+  error = BN_mod_exp(res, dsa->g, dsa->priv_key, dsa->p, ctx);
+  if ( error == 0 ) {
+    if (verbose > 0)
+      fprintf(stderr, "warn: failed to check DSA key.\n");
+    goto free_ctx;
+  }
+  if ( BN_cmp(res, dsa->pub_key) != 0 ) {
+    if (verbose > 0)
+      fprintf(stderr, "warn: invalid DSA key.\n");
+    goto free_ctx;
+  }
+  BN_clear_free(res);
+  BN_CTX_free(ctx);
 
-	fprintf(stderr, "[X] Valid DSA key found.\n");
+  fprintf(stderr, "[X] Valid DSA key found.\n");
 
-	return 0;
+  return 0;
+
+'''
+def extract_rsa_key(rsa):
+  return ""
+def extract_dsa_key(dsa):
+  return ""
+
+def write_rsa_key(rsa,suffix):
+  return ""
+def write_dsa_key(dsa,suffix):
+  return ""
 
 
-free_ctx:
-	BN_CTX_free(ctx);
-free_res:
-	BN_free(res);
-	if ( dsa->r    != NULL ) { free(dsa->r); }
-	if ( dsa->kinv != NULL ) { free(dsa->kinv); }
-	free(dsa->g); 
-free_q:
-	free(dsa->q);
-free_p:
-	free(dsa->p);
-free_pub_key:
-	free(dsa->pub_key);
-free_priv_key:
-	free(dsa->priv_key);
+def find_keys(process,stackmap):
 
-	return -1;
+  mappings= readProcessMappings(process)
+  log.info("scanning 0x%lx --> 0x%lx %s"%(stackmap.start,stackmap.end,stackmap.pathname) )
+  #openssl=cdll.LoadLibrary("libssl.so")
+  rsa=model.RSA()
+  dsa=model.DSA()
+  
+  ## stackmap.search(bytestr) // won't cut it.
+  ## process.readBytes() is ok
+  ### copy data in rsa struct
+  #### test if rsa struct is ok ?
+  ##### extract rsa key
+  ###### save it
 
-}
+  ### do the same for dsa.
 
-void free_dsa_key(DSA *dsa) {
-    if ( dsa->r    != NULL ) { free(dsa->r); }
-    if ( dsa->kinv != NULL ) { free(dsa->kinv); }
-    free(dsa->g); 
-    free(dsa->q);
-    free(dsa->p);
-    free(dsa->pub_key);
-    free(dsa->priv_key);
-}
+  data=stackmap.start  
+  ## todo change 4 by len char *
+  ##if ( j <= map->size - sizeof(RSA) ) {
+  plen=ctypes.sizeof(ctypes.c_char_p)
+  rsalen=ctypes.sizeof(rsa)
+  dsalen=ctypes.sizeof(dsa)
+  # parse for rsa
+  for j in range(stackmap.start, stackmap.end-rsalen, plen):
+    log.debug("checking 0x%lx"% j)
+    log.debug("coping 0x%lx to 0x%lx for %d bytes"% (j,ctypes.addressof(rsa),rsalen))
+    ctypes.memmove(ctypes.addressof(rsa),j,rsalen)
+    log.debug("copied 0x%lx to 0x%lx for %d bytes"% (j,ctypes.addressof(rsa),rsalen))
+    # check if data matches
+    if rsa.isValid(mappings): # refreshing would be better
+      data=extract_rsa_key(rsa)
+      if ( data is not None):
+        print "found RSA key @ 0x%lx"%(j)
+        write_rsa_key(rsa, "id_rsa")
+        continue
 
-/* }}} */
+  #do the same with dsa
+  for j in range(stackmap.start, stackmap.end-dsalen, plen):
+    log.debug("checking 0x%lx"% j)
+    log.debug("coping 0x%lx to 0x%lx for %d bytes"% (j,ctypes.addressof(dsa),dsalen))
+    ctypes.memmove(ctypes.addressof(dsa),j,dsalen)
+    log.debug("copied 0x%lx to 0x%lx for %d bytes"% (j,ctypes.addressof(dsa),dsalen))
+    # check if data matches
+    if dsa.isValid(mappings): # refreshing would be better
+      data=extract_dsa_key(dsa)
+      if ( data is not None):
+        print "found DSA key @ 0x%lx"%(j)
+        write_dsa_key(dsa, "id_dsa")
+        continue
+  
+  return
 
-static void find_keys(mapping_t *map, unsigned int off, unsigned int end)
-{
-	const char *data;
-	proc_t *p;
-	unsigned int j;
-	RSA rsa;
-	DSA dsa;
+def usage(txt):
+  log.error("Usage : %s [-v] [-a from[-to]] pid"% txt)
+  sys.exit(-1)
 
-	p = map->proc;
-  	data = map->data;
 
-	if (verbose > 0) {
-		printf("scanning 0x%lx --> 0x%lx %s\n",
-					map->address+off, map->address+end, map->name);
-	}
+def main(argv):
+  logging.basicConfig(level=logging.DEBUG)
+  logging.debug(argv)
+  if ( len(argv) < 1 ):
+    usage(argv[0])
+    print 'aa'
+    return
+  #
+  verbose = 0
+  start = 0
+  to = 0
+  #min_key_size = (sizeof(RSA) < sizeof(DSA)) ? sizeof(RSA) : sizeof(DSA);
 
-	for ( j = off; j <= end; j+=sizeof(char*)) {
+  # use optarg on v, a and to
+  pid = int(argv[0])
+  log.error("Target has pid %d"%pid)
 
-		if (verbose > 1)
-			printf("checking 0x%lx\n", map->address+j);
+  dbg=PtraceDebugger()
+  process=dbg.addProcess(pid,is_attached=False)
+  if process is None:
+    log.error("Error initializing Process debugging for %d"% pid)
+    return
+  
+  stack=process.findStack()
+  
+  # if ( dbg_get_memory(&p) )
+  # check memory access ?
+  ## check args -a and -to
+  ##  check if -a and -to is in process.
+  if start !=0 and not process.contains(start):
+    log.error("0x%lx not mapped into process %d"%( start, pid) )
+  if to != 0 and   not process.contains(to):
+    log.error("0x%lx not mapped into process %d"%( to, pid) )
+  if start !=0 and (to <= start) :
+    log.error("bad memory range")
+  off_end = stack.end - stack.start
 
-		if ( j <= map->size - sizeof(RSA) ) {
-			memcpy(&rsa, data + j, sizeof(RSA));
+  #cache it .. yeah... sometime
+  #if (dbg_map_cache(map) < 0)
+  find_keys(process,stack)
 
-			if ( might_be_RSA(&rsa, p) == 0 ) { 
+  # dbg_map_for_each(&p, map)  ????
 
-				if ( ! extract_rsa_key(&rsa, p) ) {
-					printf("found RSA key @ 0x%lx\n", map->address+j);
-					write_rsa_key(&rsa, "id_rsa");
-					free_rsa_key(&rsa);
-					continue ;
-				}
-			}
-		}
+  log.info("done for pid %d"%pid)
 
-		if ( j <= map->size - sizeof(DSA) ) {
-			memcpy(&dsa, data + j, sizeof(DSA));
+  return -1
 
-			//printf("==A==\n");
-			if ( might_be_DSA(&dsa, p) == 0 ) {
-			//printf("==B==\n");
 
-				if ( ! extract_dsa_key(&dsa, p) > 0 ) {
-					printf("found DSA key @ 0x%lx\n", map->address+j);
-					write_dsa_key(&dsa, "id_dsa");
-					free_dsa_key(&dsa);
-					continue ;
-				}
-			}
-		}
-	}
-}
+if __name__ == "__main__":
+  main(sys.argv[1:])
 
-static void usage(char *n)
-{
-	fprintf(stderr, "Usage : %s [-v] [-a from[-to]] pid\n", n);
-	exit(EXIT_FAILURE);
-}
-
-int main(int argc, char **argv) {
-	pid_t pid;
-	proc_t p;
-	int i;
-	char *end;
-	mapping_t *map, *map2;
-	unsigned int off_start, off_end, min_key_size;
-	unsigned long from, to;
-
-	if ( argc < 2 )
-		usage(argv[0]);
-
-	verbose = 0;
-	from = 0;
-	to = 0;
-	min_key_size = (sizeof(RSA) < sizeof(DSA)) ? sizeof(RSA) : sizeof(DSA);
-
-	for (i=1; i<argc-1; ++i) {
-		if ((argv[i][0] != '-') || !argv[i][1] || argv[i][2])
-			usage(argv[0]);
-
-		switch (argv[i][1]) {
-			case 'v':
-				++verbose;
-				break;
-			case 'a':
-				from = strtoul(argv[++i], &end, 16);
-				if (end) {
-					if (!*end)
-						to = from + min_key_size;
-					else if (*end != '-')
-						usage(argv[0]);
-					else if (!*++end)
-						to = 0;
-					else
-						to = strtoul(end, NULL, 16);
-				}
-				if ((from|to) & (sizeof(char *)-1)) {
-					fprintf(stderr, "error: unaligned map range\n");
-					return EXIT_FAILURE;
-				}	
-				break;
-			default:
-				usage(argv[0]);
-		}
-	}
-
-	pid = (pid_t) atoi(argv[i]);
-	fprintf(stderr, "Target has pid %d\n", pid);
-
-	if ( dbg_init(&p, pid) ) {
-		fprintf(stderr, "Error initializing proc_t for %d\n", pid);
-		return EXIT_FAILURE;
-	}
-
-	if ( dbg_attach(&p, 0) ) {
-		dbg_exit(&p);
-		return EXIT_FAILURE;
-	}
-
-	if (from) {
-		if ( dbg_get_memory(&p) )
-			err(&p, "unable to fetch memory for process %d", pid);
-		map = dbg_map_lookup_by_address(&p, from, &off_start);
-		if (!map)
-			err(&p, "0x%lx not mapped into process %i", from, pid);
-		if (to) {
-			map2 = dbg_map_lookup_by_address(&p, to, &off_end);
-			if (!map2)
-				err(&p, "0x%lx not mapped into process %i",	to, pid);
-			if (to <= from)
-				err(&p, "bad memory range");
-			if (map != map2)
-				err(&p, "0x%lx and 0x%lx not mapped in same page", from, to);
-		} else {
-			off_end = map->size;
-		}
-		if (dbg_map_cache(map) < 0)
-			err(&p, "failed to map 0x%lx", from);
-
-		find_keys(map, off_start, off_end-min_key_size);
-
-	} else {
-		if ( dbg_get_memory(&p) )
-			err(&p, "unable to fetch memory for process %d", pid);
-
-		dbg_map_for_each(&p, map) {
-
-			if (!map->data)
-				continue;
-
-			find_keys(map, 0, map->size-min_key_size);
-		}
-	}
-	fprintf(stderr, "done for pid %d\n", p.pid);
-
-	dbg_detach(&p);
-	dbg_exit(&p);
-
-	return EXIT_SUCCESS;
-}
--- model.py
+++ model.py
@@ -6,6 +6,7 @@
 
 __author__ = "Loic Jaquemet loic.jaquemet+python@gmail.com"
 
+import ctypes
 
 class mapping:
   '''	struct _mapping *next;
@@ -13,7 +14,7 @@ class mapping:
   unsigned int size;
   int flags;
   const char *name;
-  proc_t *proc;
+  mappings_t *mappings;
   char * data;
   '''
   next=None
@@ -21,7 +22,7 @@ class mapping:
   size=0
   flags=0
   name=None
-  proc=0
+  mappings=0
   data=None
   def __init__(self,address,size,flags,name):
     self.address=address
@@ -29,6 +30,124 @@ class mapping:
     self.flags=flags
     self.name=name
       
-class proc:
+class mappings:
   pid=None
   maps=None
+
+#ok
+class BIGNUM(ctypes.Structure):
+	_fields_ = [
+	("d",ctypes.c_ulong), #BN_ULONG
+	('top',ctypes.c_int),
+	('dmax',ctypes.c_int),
+	('neg',ctypes.c_int),
+	('flags',ctypes.c_int)]
+
+#ok
+class STACK(ctypes.Structure):
+	_fields_ = [
+	("num",ctypes.c_int),
+	("data",ctypes.c_char_p),
+	("sorted",ctypes.c_int),
+	("num_alloc",ctypes.c_int),
+	("comp",ctypes.POINTER(ctypes.c_int) ) ]
+
+#ok
+class CRYPTO_EX_DATA(ctypes.Structure):
+	_fields_ = [
+	("sk",ctypes.POINTER(STACK) ),
+	("dummy",ctypes.c_int)]
+	
+#ok
+class BN_MONT_CTX(ctypes.Structure):
+	_fields_ = [
+	("ri",ctypes.c_int),
+	("RR",BIGNUM),
+	("N",BIGNUM),
+	("Ni",BIGNUM),
+	("n0",ctypes.c_ulong),
+	("flags",ctypes.c_int)]
+
+
+
+''' returns 0 if the address seems to be valid
+          -1 else.
+'''
+def is_valid_address(addr,mappings):
+  '''static int is_valid_address(unsigned long addr, mappings_t *mappings) {'''
+  for m in mappings:
+    if addr in m:
+      return True
+  return False
+
+
+#KO
+class RSA(ctypes.Structure):
+  _fields_ = [
+  ("pad",	ctypes.c_int), 
+	("version",	ctypes.c_long),
+	("meth",ctypes.POINTER(BIGNUM)),#const RSA_METHOD *meth;
+	("engine",ctypes.POINTER(BIGNUM)),#ENGINE *engine;
+	('n', ctypes.POINTER(BIGNUM) ),
+	('e', ctypes.POINTER(BIGNUM) ),
+	('d', ctypes.POINTER(BIGNUM) ),
+	('p', ctypes.POINTER(BIGNUM) ),
+	('q', ctypes.POINTER(BIGNUM) ),
+	('dmp1', ctypes.POINTER(BIGNUM) ),
+	('dmq1', ctypes.POINTER(BIGNUM) ),
+	('iqmp', ctypes.POINTER(BIGNUM) ),
+	("ex_data", CRYPTO_EX_DATA ),
+	("references", ctypes.c_int),
+	("flags", ctypes.c_int),
+	("_method_mod_n", ctypes.POINTER(BN_MONT_CTX) ),
+	("_method_mod_p", ctypes.POINTER(BN_MONT_CTX) ),
+	("_method_mod_q", ctypes.POINTER(BN_MONT_CTX) ),
+	("bignum_data",ctypes.c_char_p),
+	("blinding",ctypes.POINTER(BIGNUM)),#BN_BLINDING *blinding;
+	("mt_blinding",ctypes.POINTER(BIGNUM))#BN_BLINDING *mt_blinding;
+	]
+  def isValid(self,mappings):
+    return (self.pad and self.version and
+        	(self.references < 0) and (self.references > 0xff)  and
+        is_valid_address( ctypes.addressof(self.n), mappings)    and
+        is_valid_address( ctypes.addressof(self.e), mappings)    and
+        is_valid_address( ctypes.addressof(self.d), mappings)    and
+        is_valid_address( ctypes.addressof(self.p), mappings)    and
+        is_valid_address( ctypes.addressof(self.q), mappings)    and
+        is_valid_address( ctypes.addressof(self.dmp1), mappings) and
+        is_valid_address( ctypes.addressof(self.dmq1), mappings) and
+        is_valid_address( ctypes.addressof(self.iqmp), mappings)  )
+
+#KO
+class DSA(ctypes.Structure):
+  _fields_ = [
+  ("pad",	ctypes.c_int), 
+	("version",	ctypes.c_long),
+	("write_params",ctypes.c_int),
+	('p', ctypes.POINTER(BIGNUM) ),
+	('q', ctypes.POINTER(BIGNUM) ),
+	('g', ctypes.POINTER(BIGNUM) ),
+	('pub_key', ctypes.POINTER(BIGNUM) ),
+	('priv_key', ctypes.POINTER(BIGNUM) ),
+	('kinv', ctypes.POINTER(BIGNUM) ),
+	('r', ctypes.POINTER(BIGNUM) ),
+	("flags", ctypes.c_int),
+	("_method_mod_p", ctypes.POINTER(BN_MONT_CTX) ),
+	("references", ctypes.c_int),
+	("ex_data", CRYPTO_EX_DATA ),
+	("meth",ctypes.POINTER(BIGNUM)),#	const DSA_METHOD *meth;
+	("engine",ctypes.POINTER(BIGNUM))#ENGINE *engine;
+  ]
+  def isValid(self,mappings):
+    return (
+        self.pad and self.version and
+        (self.references < 0) and (self.references > 0xff)  and
+        is_valid_address( ctypes.addressof(self.p), mappings)        and
+        is_valid_address( ctypes.addressof(self.q), mappings)        and
+        is_valid_address( ctypes.addressof(self.g), mappings)        and
+        is_valid_address( ctypes.addressof(self.priv_key), mappings) and
+        is_valid_address( ctypes.addressof(self.pub_key), mappings)  and
+        is_valid_address( ctypes.addressof(self.kinv), mappings) and
+        is_valid_address( ctypes.addressof(self.r), mappings)  )
+  
+  
