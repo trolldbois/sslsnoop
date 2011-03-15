01d057a5fcce945a01f49ba9dc0dac82ffbc2701
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2011-03-15 15:07:58 -0600

    argparse for generic struct search

--- abouchet.py
+++ abouchet.py
@@ -18,6 +18,7 @@ from ptrace.ctypes_libc import libc
 from ptrace.debugger.debugger import PtraceDebugger
 from ptrace.debugger.memory_mapping import readProcessMappings
 
+import argparse
 
 
 log=logging.getLogger('abouchet')
@@ -145,75 +146,69 @@ def hasValidPermissions(memmap):
   perms=memmap.permissions
   return (perms[0] == 'r' and perms[1] == 'w') or (perms[3] == 's')
 
-def usage(txt):
-  log.error("Usage : %s [-v] [-a from[-to]] pid"% txt)
+def usage(parser):
+  parser.print_help()
   sys.exit(-1)
 
+
+def argparser():
+  rootparser = argparse.ArgumentParser(prog='StructFinder', description='Parse memory structs.')
+  subparsers = rootparser.add_subparsers(help='sub-command help')
+  search_parser = subparsers.add_parser('search', help='search help')
+  search_parser.add_argument('pid', type=int, help='Target PID')
+  search_parser.add_argument('structType', type=str, help='Structure type name')
+  search_parser.set_defaults(func=search)
+  #
+  refresh_parser = subparsers.add_parser('refresh', help='refresh help')
+  refresh_parser.add_argument('pid', type=int, help='Target PID')
+  refresh_parser.add_argument('structType', type=str, help='Structure type name')
+  refresh_parser.add_argument('addr', type=str, help='Structure memory address')
+  refresh_parser.set_defaults(func=refresh)
+  #
+  return rootparser
+
+
+def getKlass(name):
+  module,sep,kname=name.rpartition('.')
+  mod = __import__(module, globals(), locals(), [kname])
+  klass = getattr(mod, kname)
+  return klass
+
+def search(args):
+  pid=int(args.pid)
+  structType=getKlass(args.structType)
+
+  finder = StructFinder(pid)  
+  outs=finder.find_struct( structType, maxNum=1)
+  for ss, addr in outs:
+    pass
+  #s=['0x%lx,'.join()
+  return
+
+
+def refresh(args):
+  pid=int(args.pid)
+  addr=int(args.addr,16)
+  structType=getKlass(args.structType)
+
+  finder = StructFinder(pid)  
+  instance,validated = finder.loadAt(addr, structType)
+  print instance.toString()
+  return
 
 def main(argv):
   logging.basicConfig(level=logging.INFO)
   logging.debug(argv)
-  if ( len(argv) < 1 ):
-    usage(sys.argv[0])
-    return
-  #
-  verbose = 0
-  start = 0
-  to = 0
-  #min_key_size = (sizeof(RSA) < sizeof(DSA)) ? sizeof(RSA) : sizeof(DSA);
-
-  # use optarg on v, a and to
-  pid = int(argv[0])
-  log.error("Target has pid %d"%pid)
-
-  finder = StructFinder(pid)
-
-  #### force offset
-  if len(argv) == 2:
-    addr=int(argv[1],16)
-    instance,validated = finder.loadAt(addr, ctypes_openssh.session_state)
-    print instance
-    return
   
-  if (False):
-    #When we have args ...
-    stack=finder.process.findStack()
-    # if ( dbg_get_memory(&p) )
-    # check memory access ?
-    ## check args -a and -to
-    ##  check if -a and -to is in process.
-    if start !=0 and not process.contains(start):
-      log.error("0x%lx not mapped into process %d"%( start, pid) )
-    if to != 0 and   not process.contains(to):
-      log.error("0x%lx not mapped into process %d"%( to, pid) )
-    if start !=0 and (to <= start) :
-      log.error("bad memory range")
-    off_end = stack.end - stack.start
-
-  #cache it .. yeah... sometime
-  #if (dbg_map_cache(map) < 0)
-  ### t-t-t-t, search in all MemoryMap
-  outs=finder.find_struct( ctypes_openssh.session_state, maxNum=1)
-  for ss, addr in outs:
-    #print ss.toString()
-    #print '---------'
-    #print 'Cipher name : ', ss.receive_context.cipher.contents.name
-    #print ss.receive_context.evp
-    #print 'Cipher name : ', ss.send_context.cipher.contents.name
-    #print ss.send_context.evp
-    #print 'receive context Cipher : ', ss.receive_context.cipher.contents
-    #print 'send context    Cipher : ', ss.send_context.cipher.contents
-    app_data=ss.receive_context.getEvpAppData()
-    print '"aes_counter" : "'+','.join(["0x%lx"%(val) for val in app_data.aes_counter ])+'"\n'
-    #print 'receive context Cipher app_data: ', app_data.toString()
-    (rd_key,rounds)=ss.receive_context.getEvpAppData().getCtx()
-    #print 'rounds', rounds
-    #print 'send context    Cipher app_data: ', ctypes_openssh.getEvpAppData(ss.send_context).toString()
-    #print ss.newkeys[0].contents.toString()
-    pass
-      
-  log.info("done for pid %d"%pid)
-
+  parser = argparser()
+  opts = parser.parse_args(argv)
+  try:
+    opts.func(opts)
+  except ImportError,e:
+    log.error('Struct type does not exists.')
+    print e
+  
+  log.info("done for pid %d"%opts.pid)
   return 0
 
 
