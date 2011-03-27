5b15f3ca56899dc58c76f21b49c6bcaf1966477c
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2011-03-26 21:46:50 -0600

    add hmac

--- ctypes_openssl.h
+++ ctypes_openssl.h
@@ -1,11 +1,16 @@
 
 
-
+#include "evp.h"
 #include "aes.h"
 #include "rc4.h"
 #include "blowfish.h"
+#include "des.h"
+#include "cast.h"
+
+#include "hmac.h"
+
 #include "dsa.h"
 #include "rsa.h"
-#include "evp.h"
+
 
 
