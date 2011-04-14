c35bba96a0c3057d053acb82b3d36045286ae919
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2011-04-14 05:19:39 -0600

    bump v0.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from glob import glob
 
 setup(name="sslsnoop",
-    version="0.6",
+    version="0.7",
     description="Dumps the live traffic of an ssl-encrypted stream.",
     long_description=open('README').read(),
 
