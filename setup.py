573af3b800e36cccd0b2bc379c276e6eb74b9d24
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2011-07-08 11:33:03 +0200

    use new haystack 0.8 api

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from glob import glob
 
 setup(name="sslsnoop",
-    version="0.7.1",
+    version="0.8",
     description="Dumps the live traffic of an ssl-encrypted stream.",
     long_description=open('README').read(),
 
@@ -24,5 +24,5 @@ setup(name="sslsnoop",
     author_email="loic.jaquemet+python@gmail.com",
     packages = ['sslsnoop'],
     scripts = ['scripts/sslsnoop-openssh', 'scripts/sslsnoop-openssl', 'scripts/sslsnoop'],
-    install_requires = [ "haystack >= 0.7","psutil >= 0.1"], # python-scapy, pypcap neither are in pypi... deadlink
+    install_requires = [ "haystack >= 0.8","psutil >= 0.1"], # python-scapy, pypcap neither are in pypi... deadlink
 )
